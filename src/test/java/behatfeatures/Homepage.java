379712adbf7256988c9fd3afc5f71b747c56182f
sangajala <sriram.angajala@gmail.com>
2014-09-27 15:35:40 +0100

    Homepage

--- src/test/java/behatfeatures/Homepage.java
+++ src/test/java/behatfeatures/Homepage.java
@@ -1,19 +1,24 @@
 package behatfeatures;
 
 import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+
 import cucumber.api.java.en.When;
-import junit.framework.Assert;
+import org.junit.Assert;
 import org.junit.After;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.lang.reflect.Array;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-import static junit.framework.Assert.*;
+
 
 /**
  * Created by SRI on 25-Sep-14.
@@ -36,14 +41,174 @@ public class Homepage {
         driver.quit();
     }
 
-    @Given("^consumer is in 'Home' page$")
-    public void consumer_is_in_the_HomePage() throws Throwable {
+
+
+
+    @Given("^consumer is in Home page$")
+    public void consumer_is_in_the_HomePage() {
         driver.get(homepage_url);
     }
-    @Then("^the slider automatically switches every three seconds$")
-    public void the_slider_automatically_switches_every_three_seconds() {
+
+    @Then ("^the slider automatically switches every three seconds$")
+    public void the_slider_automatically_switches_every_three_seconds() throws  Throwable{
+        WebElement image=driver.findElement(By.xpath(".//*[@id='js-index-slider']/div/div[2]/a/img"));
+        String text=image.getText();
+        Thread.sleep(3000);
+        String text1=image.getText();
+        Thread.sleep(3000);
+        String text2=image.getText();
+        String img[]=new String[3];
+        img[0]=text;
+        img[1]=text1;
+        img[2]=text2;
+
+    }
+    WebElement deal1 = driver.findElement(By.xpath(".//*[@id='home-slide-side-position-1']"));
+    WebElement deal2 = driver.findElement(By.xpath(".//*[@id='home-slide-side-position-2']"));
+    WebElement deal3 = driver.findElement(By.xpath(".//*[@id='home-slide-side-position-3']"));
+    String dealText1 = deal1.getText();
+    String dealText2 = deal2.getText();
+    String dealText3 = deal3.getText();
+
+    @When("^consumer selects deal1 in current deals highlights$")
+    public void comsumer_selects_deal1_in_current_deals_highlights() {
+        deal1.click();
+        System.out.println(dealText1);
+    }
+
+    @Then("^page navigates to Schnäppchen page$")
+    public void page_navigates_to_Schnäppchen_page() {
+        String url=driver.getPageSource();
+        Assert.assertTrue(url.contains("Schnäppchen"));
+
+    }
+
+    @And("^the corresponding deal1 page appears$")
+    public void the_corresponding_deal1_page_appears() {
+        Assert.assertTrue(driver.findElement(By.linkText(dealText1)).isDisplayed());
+
+    }
+    @When("^consumer selects Home page$")
+    public void consumer_selects_Home_page(){
+
+    }
+
+
+    @And("^consumer selects deal2 in current deals highlights$")
+    private void comsumer_selects_deal2_in_current_deals_highlights() {
+        deal2.click();
+        System.out.println(dealText2);
+    }
+
+    @And("^the corresponding deal2 page appears$")
+    public void the_corresponding_deal2_page_appears() {
+        Assert.assertTrue(driver.findElement(By.linkText(dealText2)).isDisplayed());
+
+    }
+
+
+    @And("^consumer selects deal3 in current deals highlights$")
+    public void comsumer_selects_deal3_in_current_deals_highlights() {
+        deal3.click();
+        System.out.println(dealText3);
+    }
+
+    @And("^the corresponding deal3 page appears$")
+    public void the_corresponding_deal3_page_appears() {
+        Assert.assertTrue(driver.findElement(By.linkText(dealText3)).isDisplayed());
+
+    }
+
+    @Then("^listings must appear with headings \"(.*?)\"$")
+    public void listings_must_appear_with_headings(String List) throws Throwable {
+        Assert.assertTrue(driver.findElement(By.linkText(List)).isDisplayed());
+    }
+
+    @Then("^Beliebteste Schnäppchen listing should have elements$")
+    public void Beliebteste_Schnäppchen_listing_should_have_elements() throws Throwable{
+        if(driver.findElements(By.id("js-sorted-bargain")).size() != 0){
+            System.out.println("Element is Present");
+        }else{
+            System.out.println("Element is Absent");
+        }
+
+    }
+    @Then("^Beliebteste Gutscheine listing should have elements$")
+    public void Beliebteste_Gutscheine_listing_should_have_elements() throws Throwable{
+        if(driver.findElements(By.xpath("")).size() != 0){
+            System.out.println("Element is Present");
+        }else{
+            System.out.println("Element is Absent");
+        }
+
+    }
+
+    @Then("^Aktuelle Schnäppchen listing should have elements$")
+    public void Aktuelle_Schnäppchen_listing_should_have_elements() throws Throwable{
+        if(driver.findElements(By.xpath("")).size() != 0){
+            System.out.println("Element is Present");
+        }else{
+            System.out.println("Element is Absent");
+        }
+    }
+
+    @Then("^Top-Ten Gutscheine listing should have elements$")
+    public void Top_Ten_Gutscheine_listing_should_have_elements() throws Throwable{
+        if(driver.findElements(By.xpath("")).size() != 0){
+            System.out.println("Element is Present");
+        }else{
+            System.out.println("Element is Absent");
+        }
+    }
+
+    @When("^selects element form Beliebteste Schnäppchen listing$")
+    public void selects_element_from_Beliebteste_Schnäppchen_listing(){
+        List<WebElement> element = driver.findElements(By.xpath(".//*[@id='js-sorted-bargain']"));
+        if(element.size()>0){
+        }
+    }
+    @Then("^corresponding Beliebteste Schnäppchen page appears$")
+    public void corresponding_Beliebteste_Schnäppchenpage_appears()throws Throwable{
+
+
+    }
+    @When("^selects element form Beliebteste Gutscheine listing$")
+    public void selects_element_from_Beliebteste_Gutscheine_listing() {
+    }
+    @Then("^corresponding Beliebteste Gutscheine page appears$")
+    public void corresponding_Beliebteste_Gutscheine_appears()throws Throwable{
+
+
+    }
+
+
+    @When("^selects element form Aktuelle Schnäppchen listing$")
+    public void selects_element_from_Aktuelle_Schnäppchen_listing(){
+    }
+    @Then("^corresponding Aktuelle Schnäppchen page appears$")
+    public void corresponding_Aktuelle_Schnäppchen_page_appears()throws Throwable{
+
+
+    }
+    @When("^select all elements one after one form Top-Ten Gutscheine listing$")
+    public void select_all_elements_one_after_one_from_Top_Ten_Gutscheine_listing(){
+    }
+    @Then("^corresponding pages appear$")
+    public void corresponding_pages_appear()throws Throwable{
+
+
+    }
+    @When ("^consumer selects the link \"(.*?)\"$")
+    public void consumer_selects_the_link(String Link) throws Throwable{
+        WebElement link=driver.findElement(By.linkText(Link));
+        link.click();
+
+    }
+    @Then("corresponding \"(.*?)\" page appears")
+    public void correspondin_page_appears(String Link) throws Throwable{
+        String url=driver.getPageSource();
+        Assert.assertTrue(url.contains(Link));
     }
 
 }
-
 
--- src/test/resources/behatfeatures/HomePage.feature
+++ src/test/resources/behatfeatures/HomePage.feature
@@ -1,34 +1,61 @@
 @home
+
 Feature: Home page functionality
-
   As a consumer
-  I want to check for current deals highlights
-  So I can get benefit of the latest deals
-  Background: Given user is in 'Home' page
+  I want to check for current deals ,vouchers and coupons
+  So I can get benefit of best suitable deal
+  Background:
+    Given consumer is in Home page
 
-  Scenario: consumer can view the current deals highlights
-    Given consumer is in 'Home' page
-    //When consumer checks for 'current deals highlights'
-    // the deals appears in slider
+  Scenario:Consumer can view the current deal highlights in slider
     Then the slider automatically switches every three seconds
 
+  Scenario : consumer navigates to the deal page from slider
+    When consumer selects deal1 in current deals highlights
+    Then page navigates to Schnäppchen page
+    And the corresponding deal1 page appears
+    When consumer selects Home page
+    And consumer selects deal2 in current deals highlights
+    Then page navigates to Schnäppchen page
+    And the corresponding deal2 page appears
+    When consumer selects Home page
+    And consumer selects deal3 in current deals highlights
+    Then page navigates to Schnäppchen page
+    And the corresponding deal3 page appears
 
-  // Scenario: consumer can view particular deal in current deals highlights
-  //  Given consumer is in 'Home' page
-  //  When consumer moves cursor over the slider element
-  //  Then the slider stops
-  //  And the deal present in that slider element is highlighted
-    //
 
-  Scenario Outline: consumer navigates to the deal page
-    Given consumer is in 'Home' page
-    When consumer select <Deal> in current deals highlights
-    Then page navigates to Bargain page
-    And the corresponding deal <Deal> page appears
-
-
+  Scenario Outline: Consumer can view listings in the home page
+    Then listings must appear with headings <List>
   Examples:
-    |Deal  |
-    |Deal1 |
-    |Deal2 |
-    |Deal3 |
+    |List                     |
+    |"Beliebteste Schnäppchen"|
+    |"Beliebteste Gutscheine" |
+    |"Aktuelle Schnäppchen"   |
+    |"Top-Ten Gutscheine"     |
+
+  Scenario: Listings must have elements
+    Then Beliebteste Schnäppchen listing should have elements
+    Then Beliebteste Gutscheine listing should have elements
+    Then Aktuelle Schnäppchen listing should have elements
+    Then Top-Ten Gutscheine listing should have elements
+
+  Scenario : Consumer can view the deals present in listings
+    When selects element form Beliebteste Schnäppchen listing
+    Then corresponding Beliebteste Schnäppchen page appears
+    When selects element form Beliebteste Gutscheine listing
+    Then corresponding Beliebteste Gutscheine page appears
+    When selects element form Aktuelle Schnäppchen listing
+    Then corresponding Aktuelle Schnäppchen page appears
+    When select all elements one after one form Top-Ten Gutscheine listing
+    Then corresponding pages appear
+
+  Scenario Outline: Consumer can view static links
+    When consumer selects the link <Link>
+    Then corresponding <Link> page appears
+  Examples:
+    | Link         |
+    |"Impressum"   |
+    |"Datenschutz" |
+    |"Hilfe"       |
+    |"Presse"      |
+
